784702c66f05fdc290de25ea7ba9d3b4be8d0416
1218AND <1218AND@6A-12>
2014-04-16 14:33:40 +0900

    CommonUtilities DemoActivity コメントを追記

--- src/com/google/android/gcm/demo/app/CommonUtilities.java
+++ src/com/google/android/gcm/demo/app/CommonUtilities.java
@@ -22,22 +22,35 @@ import android.content.Intent;
  * Helper class providing methods and constants common to other classes in the
  * app.
  */
+
+/**
+ *  ユーティリティクラスは、staticな共通の処理のメソッドを集めたクラスです。
+ *　同じ処理が2箇所以上で出てきた場合は、ユーティリティクラスにまとめることができる可能性があります。
+ *　ユーティリティクラスを使うと共通処理を1箇所にまとめることができますので、
+ *  再利用性の向上ばかりでなく、あとから修正が発生してもユーティリティクラスの修正だけですみ、
+ *  保守性が向上するというメリットがあります。
+ * */
+
 public final class CommonUtilities {
 
     /**
      * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
+     * GAEのユニークなURL、末尾の/は削除しておく必要がある
      */
-    static final String SERVER_URL = "http://127.0.0.1:8080/gcm-demo";
+    static final String SERVER_URL = "http://1-dot-ayaseya-dev-jp.appspot.com";
 
     /**
      * Google API project id registered to use GCM.
+     * Google Developers Console
+     * https://console.developers.google.com/
+     * プロジェクト番号＝SENDER_ID
      */
     static final String SENDER_ID = "977505068557";
 
     /**
      * Tag used on log messages.
      */
-    static final String TAG = "GCMDemo";
+    static final String TAG = "GCM";
 
     /**
      * Intent used to display a message in the screen.
@@ -60,6 +73,8 @@ public final class CommonUtilities {
      * @param message message to be displayed.
      */
     static void displayMessage(Context context, String message) {
+    	// Broadcastに添付するIntent、引数は一意（ユニーク）な文字列を指定
+    	// 一般的にはパッケージ名＋任意の文字列
         Intent intent = new Intent(DISPLAY_MESSAGE_ACTION);
         intent.putExtra(EXTRA_MESSAGE, message);
         context.sendBroadcast(intent);
--- src/com/google/android/gcm/demo/app/DemoActivity.java
+++ src/com/google/android/gcm/demo/app/DemoActivity.java
@@ -35,115 +35,141 @@ import com.google.android.gcm.GCMRegistrar;
  */
 public class DemoActivity extends Activity {
 
-    TextView mDisplay;
-    AsyncTask<Void, Void, Void> mRegisterTask;
+	TextView mDisplay;
+	AsyncTask<Void, Void, Void> mRegisterTask;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        checkNotNull(SERVER_URL, "SERVER_URL");
-        checkNotNull(SENDER_ID, "SENDER_ID");
-        // Make sure the device has the proper dependencies.
-        GCMRegistrar.checkDevice(this);
-        // Make sure the manifest was properly set - comment out this line
-        // while developing the app, then uncomment it when it's ready.
-        GCMRegistrar.checkManifest(this);
-        setContentView(R.layout.main);
-        mDisplay = (TextView) findViewById(R.id.display);
-        registerReceiver(mHandleMessageReceiver,
-                new IntentFilter(DISPLAY_MESSAGE_ACTION));
-        final String regId = GCMRegistrar.getRegistrationId(this);
-        if (regId.equals("")) {
-            // Automatically registers application on startup.
-            GCMRegistrar.register(this, SENDER_ID);
-        } else {
-            // Device is already registered on GCM, check server.
-            if (GCMRegistrar.isRegisteredOnServer(this)) {
-                // Skips registration.
-                mDisplay.append(getString(R.string.already_registered) + "\n");
-            } else {
-                // Try to register again, but not in the UI thread.
-                // It's also necessary to cancel the thread onDestroy(),
-                // hence the use of AsyncTask instead of a raw thread.
-                final Context context = this;
-                mRegisterTask = new AsyncTask<Void, Void, Void>() {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// nullチェック用の内部クラス、下記に定義していいます。
+		checkNotNull(SERVER_URL, "SERVER_URL");
+		checkNotNull(SENDER_ID, "SENDER_ID");
 
-                    @Override
-                    protected Void doInBackground(Void... params) {
-                        ServerUtilities.register(context, regId);
-                        return null;
-                    }
+		// Make sure the device has the proper dependencies.
+		// checkDevice() メソッドは GCM をサポートしていることを検証し、
+		// そうでない場合 ( 例えば、デバイスが Google APIs を含んでいないエミュレータの場合など ) は、
+		// 例外をスローします。
+		GCMRegistrar.checkDevice(this);
+		
+		// Make sure the manifest was properly set - comment out this line
+		// while developing the app, then uncomment it when it's ready.
+		// checkManifest() メソッドは、アプリケーションのマニフェストが
+		// すべての要件を満たしているものを含んでいることを検証します。
+		GCMRegistrar.checkManifest(this);
+		
+		setContentView(R.layout.main);
+		mDisplay = (TextView) findViewById(R.id.display);
+		
+		// registerReceiverの登録を行う
+		// 第一引数 BroadcastReceiver,第二引数 IntentFilter
+		// mHandleMessageReceiverは内部クラスとして最下部で定義しています。
+		registerReceiver(mHandleMessageReceiver, new IntentFilter(DISPLAY_MESSAGE_ACTION));
+		
+		// チェックが完了した時点で、デバイスは GCMRegsistrar.register() を呼び出し、
+		// GCM にサインアップしたときに取得した SENDER_ID を渡して登録します。
+		// しかし、GCMRegistrar のシングルトンが登録インテントの到着で登録 ID を記録していることから、
+		// GCMRegistrar.getRegistrationId() を最初に呼び出し、
+		// デバイスがすでに登録されているかどうかをチェックすることができます。
+		final String regId = GCMRegistrar.getRegistrationId(this);// デバイスが登録済みか否かのチェックのためGCMから登録IDを取得する
+		
+		if (regId.equals("")) {
+			// Automatically registers application on startup.
+			
+			// 登録IDが存在しなかった場合、GCMへSENDER_IDを送信します。
+			GCMRegistrar.register(this, SENDER_ID);
+		} else {
+			// Device is already registered on GCM, check server.
+			// 登録IDが存在した場合
+			if (GCMRegistrar.isRegisteredOnServer(this)) {
+				// Skips registration.
+				// 登録IDが存在した場合、その旨を表示します。
+				mDisplay.append(getString(R.string.already_registered) + "\n");
+			} else {
+				// Try to register again, but not in the UI thread.
+				// It's also necessary to cancel the thread onDestroy(),
+				// hence the use of AsyncTask instead of a raw thread.
+				// GCMの登録IDが存在したが、サーバーへの登録がされていないので通知します。
+				final Context context = this;
+				// 非同期処理で別スレッドに登録処理を任せます。（GUIスレッドではHTTP通信はできないため）
+				mRegisterTask = new AsyncTask<Void, Void, Void>() {
 
-                    @Override
-                    protected void onPostExecute(Void result) {
-                        mRegisterTask = null;
-                    }
+					@Override
+					protected Void doInBackground(Void... params) {
+						ServerUtilities.register(context, regId);
+						return null;
+					}
 
-                };
-                mRegisterTask.execute(null, null, null);
-            }
-        }
-    }
+					@Override
+					protected void onPostExecute(Void result) {
+						mRegisterTask = null;
+					}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-        return true;
-    }
+				};
+				mRegisterTask.execute(null, null, null);
+			}
+		}
+	}
+	// オプションメニュー
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            /*
-             * Typically, an application registers automatically, so options
-             * below are disabled. Uncomment them if you want to manually
-             * register or unregister the device (you will also need to
-             * uncomment the equivalent options on options_menu.xml).
-             */
-            /*
-            case R.id.options_register:
-                GCMRegistrar.register(this, SENDER_ID);
-                return true;
-            case R.id.options_unregister:
-                GCMRegistrar.unregister(this);
-                return true;
-             */
-            case R.id.options_clear:
-                mDisplay.setText(null);
-                return true;
-            case R.id.options_exit:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		/*
+		 * Typically, an application registers automatically, so options below
+		 * are disabled. Uncomment them if you want to manually register or
+		 * unregister the device (you will also need to uncomment the equivalent
+		 * options on options_menu.xml).
+		 */
+		/*
+		 * case R.id.options_register: GCMRegistrar.register(this, SENDER_ID);
+		 * return true; case R.id.options_unregister:
+		 * GCMRegistrar.unregister(this); return true;
+		 */
+		case R.id.options_clear:
+			mDisplay.setText(null);
+			return true;
+		case R.id.options_exit:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-    @Override
-    protected void onDestroy() {
-        if (mRegisterTask != null) {
-            mRegisterTask.cancel(true);
-        }
-        unregisterReceiver(mHandleMessageReceiver);
-        GCMRegistrar.onDestroy(this);
-        super.onDestroy();
-    }
-
-    private void checkNotNull(Object reference, String name) {
-        if (reference == null) {
-            throw new NullPointerException(
-                    getString(R.string.error_config, name));
-        }
-    }
-
-    private final BroadcastReceiver mHandleMessageReceiver =
-            new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
-            mDisplay.append(newMessage + "\n");
-        }
-    };
+	@Override
+	protected void onDestroy() {
+		if (mRegisterTask != null) {
+			mRegisterTask.cancel(true);// 処理中、処理待ちの非同期処理をキャンセルする
+		}
+		unregisterReceiver(mHandleMessageReceiver);// レシーバーの解除
+		GCMRegistrar.onDestroy(this);// 
+		super.onDestroy();
+	}
+	
+	// nullチェック用の内部クラス
+	// nullだった場合は第二引数をエラーとしたNullPointerExceptionが投げられます。
+	private void checkNotNull(Object reference, String name) {
+		if (reference == null) {
+			throw new NullPointerException(getString(R.string.error_config,
+					name));
+		}
+	}
+	
+	// BroadcastReceiver、onReceive（）に受信時の挙動を記載しています。
+	private final BroadcastReceiver mHandleMessageReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// Intentに添付された文字列情報を取り出しTextViewに追記します。
+			String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
+			mDisplay.append(newMessage + "\n");
+		}
+	};
 
 }
